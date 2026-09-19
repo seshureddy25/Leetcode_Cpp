8d7ff4cf5c52ddfc4287d0e55931a0f69451f03b
VEERAMREDDYGARI SESHU KUMAR REDDY <vskr5696@gmail.com>
2026-09-19 15:03:52 +0530

    Time: 0 ms (100%), Space: 14.7 MB (8.1%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>arr;
+        int freq[10001]={0};
+        for(int i=0;i<nums1.size();i++)
+        {
+            freq[nums1[i]]++;
+        }
+        for(int i=0;i<nums2.size();i++ )
+        {
+            if(freq[nums2[i]]!=0)
+            {
+                arr.push_back(nums2[i]);
+                freq[nums2[i]]=0;
+            }
+        }
+        return arr;
+    }
+};
